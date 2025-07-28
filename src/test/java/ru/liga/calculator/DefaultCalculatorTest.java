5a5add1fed405fc3b00d3e46c08bbf696f92dff7
mvad62 <mvad@hotmail.com>
2025-07-28 21:00:52 +0300

    Study calculator

--- src/test/java/ru/liga/calculator/DefaultCalculatorTest.java
+++ src/test/java/ru/liga/calculator/DefaultCalculatorTest.java
@@ -1,5 +1,37 @@
 package ru.liga.calculator;
 
-class DefaultCalculatorTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class DefaultCalculatorTest {
+
+    @Test
+    public void testEmptyString() {
+        StudyCalculator calculator = new StudyCalculator();
+        assertEquals(0, calculator.sum(""));
+    }
+
+    @Test
+    public void testSingleNumber() {
+        StudyCalculator calculator = new StudyCalculator();
+        assertEquals(5, calculator.sum("5"));
+    }
+
+    @Test
+    public void testTwoNumbers() {
+        StudyCalculator calculator = new StudyCalculator();
+        assertEquals(8, calculator.sum("3 5"));
+    }
+
+    @Test
+    public void testMoreThanTwoNumbers() {
+        StudyCalculator calculator = new StudyCalculator();
+        assertThrows(RuntimeException.class, () -> calculator.sum("1 2 3"));
+    }
+
+    @Test
+    public void testNonNumericCharacters() {
+        StudyCalculator calculator = new StudyCalculator();
+        assertThrows(RuntimeException.class, () -> calculator.sum("1 a"));
+    }
 }
